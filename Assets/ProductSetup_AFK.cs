8c4ca057301bd7bd55803db9798f43213cfbda77
Macchiato <simppp@gmail.com>
2023-05-17 07:03:04 +0900

    Prefab 세팅 기초 메소드 작성

--- Assets/ProductSetup_AFK.cs
+++ Assets/ProductSetup_AFK.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
 
@@ -30,9 +31,32 @@ namespace VRSuya.AvatarSettingUpdater {
 
 		/// <summary>외부의 세팅 요청을 처리하는 메인 메소드 입니다.</summary>
 		internal static void RequestSetting() {
-			if (VRSuyaAFKGameObject) {
-				UpdateParentConstraints();
-				UpdatePrefabName();
+			if (InstallProductAFK) {
+				SetupPrefab();
+				if (VRSuyaAFKGameObject) {
+					UpdateParentConstraints();
+					UpdatePrefabName();
+				}
+			}
+			return;
+		}
+
+		/// <summary>아바타에 Prefab이 있는지 검사하고 없으면 설치하는 메소드 입니다.</summary>
+		private static void SetupPrefab() {
+			string[] ChildAvatarGameObjectNames = new string[0];
+			foreach (Transform ChildTransform in AvatarGameObject.transform) {
+				ChildAvatarGameObjectNames = ChildAvatarGameObjectNames.Concat(new string[] { ChildTransform.name }).ToArray();
+				Debug.Log("[VRSuya] ChildTransform.name : " + ChildTransform.name);
+			}
+			if (!Array.Exists(ChildAvatarGameObjectNames, GameObjectName => GameObjectName.Contains("VRSuya_AFK_Prefab"))) {
+				string[] PrefabFilePaths = new string[0];
+				PrefabFilePaths = AFK.PrefabGUID.Select(AssetGUID => AssetDatabase.GUIDToAssetPath(AssetGUID)).ToArray();
+				string TargetPrefabPath = Array.Find(PrefabFilePaths, FileName => FileName.Contains("VRSuya_AFK_Prefab") && FileName.Contains(AvatarType.ToString()));
+				if (TargetPrefabPath != null) {
+					GameObject TargetPrefab = (GameObject)AssetDatabase.LoadAssetAtPath(TargetPrefabPath, typeof(GameObject));
+					GameObject TargetInstance = Instantiate(TargetPrefab);
+					TargetInstance.transform.parent = AvatarGameObject.transform;
+				}
 			}
 			return;
 		}
